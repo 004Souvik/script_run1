50e95bfaa2d495154bf9b612264e963bb7eda156
004Souvik <123533712+004Souvik@users.noreply.github.com>
2025-11-02 12:52:02 +0530

    Update pyscript.py

--- pyscript.py
+++ pyscript.py
@@ -143,4 +143,5 @@ def web_app(df1:pd.DataFrame, df2:pd.DataFrame, leng:int):
     }
 
     pls_met_df = pd.DataFrame(pls_met_dict, index=[str(i) for i in range(leng, len(x_tr.columns)+1, 1)])
-    return pls_met_df
+
+    return "Total_metrics.xlsx", pls_met_df
